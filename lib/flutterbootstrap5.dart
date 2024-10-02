4f4910dad8f7d1e9df8faa2c7021bde22c28414d
postboxat18 <aravinthc18@gmail.com>
2024-10-02 18:18:37 +0530

    commit

--- lib/flutterbootstrap5.dart
+++ lib/flutterbootstrap5.dart
@@ -8,41 +8,79 @@ import 'package:flutter/material.dart';
 import 'package:intersperse/intersperse.dart';
 
 part 'src/content/typography.dart';
+
 part 'src/layout/bootstrap_theme.dart';
+
 part 'src/layout/breakpoints.dart';
+
 part 'src/layout/fb5_style/fb5_vertical_alignment.dart';
+
 part 'src/layout/fb5_style/fb5_self_alignment.dart';
+
 part 'src/layout/fb5_style/fb5_horizontal_alignment.dart';
+
 part 'src/layout/fb5_style/fb5_col_offset.dart';
+
 part 'src/layout/fb5_style/fb5_col_size.dart';
+
 part 'src/layout/fb5_style/fb5_gutter.dart';
+
 part 'src/layout/fb5_style/fb5_margin.dart';
+
 part 'src/layout/fb5_style/fb5_order.dart';
+
 part 'src/layout/fb5_style/fb5_padding.dart';
+
 part 'src/layout/fb5_style/fb5_display.dart';
+
 part 'src/layout/fb5_style/converter.dart';
+
 part 'src/layout/fb5_style/fb5_row_cols.dart';
+
 part 'src/layout/fb5_style/fb5_style.dart';
+
 part 'src/layout/media_query_builder.dart';
+
 part 'src/utilities/borders.dart';
+
 part 'src/utilities/helper.dart';
+
 part 'src/utilities/colors.dart';
+
 part 'src/utilities/spacing.dart';
+
 part 'src/widgets/flutterbootstrap5.dart';
+
 part 'src/widgets/layout/fb5_col.dart';
+
 part 'src/widgets/layout/fb5_order.dart';
+
 part 'src/widgets/layout/fb5_grid.dart';
+
 part 'src/widgets/layout/fb5_container.dart';
+
 part 'src/widgets/layout/fb5_row.dart';
+
 part 'src/widgets/typography/display1.dart';
+
 part 'src/widgets/typography/display2.dart';
+
 part 'src/widgets/typography/display3.dart';
+
 part 'src/widgets/typography/display4.dart';
+
 part 'src/widgets/typography/display5.dart';
+
 part 'src/widgets/typography/display6.dart';
+
 part 'src/widgets/typography/h1.dart';
+
 part 'src/widgets/typography/h2.dart';
+
 part 'src/widgets/typography/h3.dart';
+
 part 'src/widgets/typography/h4.dart';
+
 part 'src/widgets/typography/h5.dart';
+
 part 'src/widgets/typography/h6.dart';
